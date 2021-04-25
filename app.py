53db64829aa2d295d297d3240fc82c91d80297fd
hungi <yuboyhenry@gmail.com>
2021-04-25 15:47:30 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 18 01:00:17 2018
+
+@author: linzino
+"""
+
+
+from flask import Flask, request, abort
+from linebot import (LineBotApi, WebhookHandler)
+from linebot.exceptions import (InvalidSignatureError)
+from linebot.models import *
+import mongodb
+import re
+
+app = Flask(__name__)
+
+# 必須放上自己的Channel Access Token
+line_bot_api = LineBotApi('dQULqUT1tNn74kOcPxwBE9HsBAqEYp0no8FcM3W9NsM18Kd68TyLP/AKh/gYbn+5lXypMLyJXXhcXwBgEDnzNUk8H7xrirgqA+cYW2sOiMjcrwIZw8NO1YkzERLLC/gcbA0sPByIkp3Wxd249D6bEgdB04t89/1O/w1cDnyilFU=')
+# 必須放上自己的Channel Secret
+handler = WebhookHandler('cc810a5bdca140c09022cfde214027a8')
+
+line_bot_api.push_message('Uba7af96e4fe1b68a071ffbd7af3c7a63', TextSendMessage(text='你可以開始了'))
+
+# 監聽所有來自 /callback 的 Post Request
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+#訊息傳遞區塊
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    ### 抓到顧客的資料 ###
+    profile = line_bot_api.get_profile(event.source.user_id)
+    uid = profile.user_id #使用者ID
+    usespeak=str(event.message.text) #使用者講的話
+    if re.match('[0-9]{4}[<>][0-9]',usespeak): # 先判斷是否是使用者要用來存股票的
+        #mongodb.write_user_stock_fountion(stock=usespeak[0:4], bs=usespeak[4:5], price=usespeak[5:])
+        #line_bot_api.push_message(uid, TextSendMessage(text=usespeak[0:4]+'已經儲存成功'))
+        print('ok')
+        message = TextSendMessage(text=event.message.text)
+        line_bot_api.reply_message(event.reply_token,message)
+        return 0
+
+    
+    elif re.match('刪除[0-9]{4}',usespeak): # 刪除存在資料庫裡面的股票
+        mongodb.delete_user_stock_fountion(stock=usespeak[2:])
+        line_bot_api.push_message(uid, TextSendMessage(usespeak+'已經刪除成功'))
+        return 0
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
